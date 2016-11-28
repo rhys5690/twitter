fbcce0718d01eb2bf3ae2e0fec89ee18a49655eb
rhys5690 <rhys5690@gmail.com>
2016-11-29 07:56:09 +1100

    Completed profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 
   devise_for :users
+  root 'pages#home'
   get '/home' => 'pages#home'
   get '/profile' => 'pages#profile'
   get '/explore' => 'pages#explore'
